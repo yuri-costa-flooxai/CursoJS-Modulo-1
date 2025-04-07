9b33b5bc99f5f9609fe88e1c9457531363f243df
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-07 11:23:43 -0300

    feat: Notes from Class-1, variables & operators

---
+++ introducao-basico-1/index.js
@@ -0,0 +1,38 @@
+console.log("Hello World!");
+
+// isso e um comentario
+
+// variaveis 
+
+// let e o modo moderno de declarar variaveis, block scoped
+let olaMundo = "Ola Mundo!";
+console.log(olaMundo);
+
+// var e o modo antigo de declarar variaveis, function scoped
+var olaMundo2 = "Ola Mundo 2!";
+
+// const e o modo moderno de declarar variaveis, block scoped, mas imutavel
+const olaMundo3 = "Ola Mundo 3!";
+// console.log(olaMundo3);
+
+// operadores
+
+let a = 10;
+let b = 5;
+
+console.log(a == b); // igualdade - sem checagem de tipo
+console.log(a === b); // igualdade - com checagem de tipo
+console.log(a != b); // desigualdade - sem checagem de tipo
+console.log(a !== b); // desigualdade - com checagem de tipo
+
+console.log(a > b); // maior que
+console.log(a < b); // menor que
+console.log(a >= b); // maior ou igual a
+console.log(a <= b); // menor ou igual a
+
+console.log(a + b); // soma
+console.log(a - b); // subtracao
+console.log(a * b); // multiplicacao
+console.log(a / b); // divisao
+console.log(a % b); // resto da divisao
+console.log(a ** b); // potencia

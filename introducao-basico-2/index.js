4c42c4d1881320be2e96b3e853fe3b3c9d796bb0
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-07 11:31:17 -0300

    feat: Notes from class-2, DOM manipulation, Arrays & OOP.

--- introducao-basico-2/index.js
+++ introducao-basico-2/index.js
@@ -18,3 +18,39 @@ const soma2 = function(a, b) {
 const soma3 = (a, b) => {
     return a + b;
 }
+
+// DOM
+
+window.addEventListener("focus", event => {
+    console.log("focus");
+});
+
+document.addEventListener("click", event => {
+    console.log("click");
+});
+
+let agora = new Date();
+console.log(agora.toLocaleDateString("pt-BR"));
+
+// Arrays
+let carros = ["Fusca", "Civic", "Palio"];
+console.log(carros[0].toUpperCase());
+
+carros.forEach((carro, index) => {
+    console.log(`${index} - ${carro}`);
+});
+
+// OOP
+class Carro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    ligar() {
+        console.log("Carro ligado");
+    }
+}
+
+let carro1 = new Carro("Fusca", "Fusca", 1975);
+console.log(carro1);
